55ef6fc17e03482c1911431e2bdb8c400131cc81
Richard Ludwig <richard@richardludwig.com>
2019-12-04 01:42:18 -0800

    updated files

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {
+  DefconFiveButton,
+  DefconFourButton,
+  DefconThreeButton,
+  DefconTwoButton,
+  DefconOneButton
+} from './constants/ButtonColors';
+
+const buttonSounds = {
+  defconFive: require('./assets/audio/defconFive.mp3'),
+  defconFour: require('./assets/audio/defconFour.mp3'),
+  defconThree: require('./assets/audio/defconThree.mp3'),
+  defconTwo: require('./assets/audio/defconTwo.mp3'),
+  defconOne: require('./assets/audio/defconOne.mp3')
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
