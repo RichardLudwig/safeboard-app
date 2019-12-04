5f18d9532d7d555308b52ad36cd27f22f64abdae
Richard Ludwig <richard@richardludwig.com>
2019-12-04 01:26:34 -0800

    added constants/ButtonColors.js

---
+++ constants/ButtonColors.js
@@ -0,0 +1,10 @@
+// DEFCON 5
+export const DefconFiveButton = '#2099CC'
+// DEFCON 4
+export const DefconFourButton = '#209902'
+// DEFCON 3
+export const DefconFourButton = '#FDCC01'
+// DEFCON 2
+export const DefconFourButton = '#9A0600'
+// DEFCON 1
+export const DefconFourButton = '#FFFFFF'
