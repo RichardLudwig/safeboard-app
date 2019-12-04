20bc8523e7cbbc9265180ae1f591c44beb16273e
Richard Ludwig <richard@richardludwig.com>
2019-12-04 02:19:27 -0800

    added expo-font module, added all button placeholder audio in app.js" "

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Audio } from 'expo-av';
 
 import {
   DefconFiveButton,
@@ -18,10 +19,69 @@ const buttonSounds = {
 }
 
 export default function App() {
+  handleAudioSound = async sound => {
+    const audioObject = new Audio.Sound()
+
+    try {
+      let source = buttonSounds[sound]
+      await audioObject.loadAsync(source)
+      await audioObject
+        .playAsync()
+        .then(async playbackStatus => {
+          setTimeout(() => {
+            audioObject.unloadAsync()
+          }, playbackStatus.playableDurationMillis)
+        }).catch(error => {
+          console.log(error);
+        })
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    	<View style={styles.buttonContainer}>
+    		<TouchableOpacity
+    			style={[styles.button, { backgroundColor: DefconFiveButton }]}
+    			onPress={() => this.handleAudioSound('defconFive')}
+    		>
+    			<Text style={styles.buttonText}>Leave me alone!</Text>
+    		</TouchableOpacity>
+    	</View>
+      <View style={styles.buttonContainer}>
+    		<TouchableOpacity
+    			style={[styles.button, { backgroundColor: DefconFourButton }]}
+    			onPress={() => this.handleAudioSound('defconFour')}
+    		>
+    			<Text style={styles.buttonText}>I have a fiance!</Text>
+    		</TouchableOpacity>
+    	</View>
+      <View style={styles.buttonContainer}>
+    		<TouchableOpacity
+    			style={[styles.button, { backgroundColor: DefconThreeButton }]}
+    			onPress={() => this.handleAudioSound('defconThree')}
+    		>
+    			<Text style={styles.buttonText}>My dad's a cop!</Text>
+    		</TouchableOpacity>
+    	</View>
+      <View style={styles.buttonContainer}>
+    		<TouchableOpacity
+    			style={[styles.button, { backgroundColor: DefconTwoButton }]}
+    			onPress={() => this.handleAudioSound('defconTwo')}
+    		>
+    			<Text style={styles.buttonText}>HELP!</Text>
+    		</TouchableOpacity>
+    	</View>
+      <View style={styles.buttonContainer}>
+    		<TouchableOpacity
+    			style={[styles.button, { backgroundColor: DefconOneButton }]}
+    			onPress={() => this.handleAudioSound('defconOne')}
+    		>
+    			<Text style={styles.buttonText}>911</Text>
+    		</TouchableOpacity>
+    	</View>
+</View>
   );
 }
 
@@ -32,4 +92,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  button: {
+    margin: 5,
+    height: '20%',
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 20
+  }
 });
